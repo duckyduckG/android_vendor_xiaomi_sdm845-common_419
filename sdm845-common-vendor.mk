bb95950c8047b5b9cfdcbb600786f022da42be17
duckyduckg <duckyduckg65@gmail.com>
2026-02-04 12:17:39 +0000

    sdm845-common: Regenerate vendor
    Change-Id: I40190ab238dd8c190e80652c3b06a52e5adaab06

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -201,6 +201,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vpp@1.2 \
     libFileMux_proprietary \
     libacdb-fts \
+    libacdbloader \
     libacdbrtac \
     libadiertac \
     libadm \
@@ -582,13 +583,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
     system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so \
-    system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so \
-    vendor_lib_libEGL_adreno_so \
-    vendor_lib_libGLESv2_adreno_so \
-    vendor_lib_libq3dtools_adreno_so \
-    vendor_lib64_libEGL_adreno_so \
-    vendor_lib64_libGLESv2_adreno_so \
-    vendor_lib64_libq3dtools_adreno_so
+    system_ext_priv-app_WfdService_lib_arm64_libwfdnative_so
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
